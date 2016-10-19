e74d340df7aa26ced5187a82fbe359d6fb312a53
shuoshuo.qin <shuoshuo.qin@tendcloud.com>
2016-10-19 12:01:37 +0800

    init my spacemacs layers.

---
+++ simon/funcs.el
@@ -0,0 +1,8 @@
+
+[simon/my-mc-mark-next-like-this]
+
+(defun simon/my-mc-mark-next-like-this ()
+  (interactive)
+  (if (region-active-p)
+      (mc/mark-next-like-this 1)
+    (er/expand-region 1)))
---
+++ simon/keybindings.el
@@ -0,0 +1,3 @@
+[kbd]
+
+(global-set-key (kbd "s-d") 'simon/my-mc-mark-next-like-this)
